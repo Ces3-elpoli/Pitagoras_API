77e5ab91c2a2aa0a917339b9ce4fdb860ac5da3f
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-04-11 19:16:40 -0500

    Se Agrega el metodo Doget

--- src/main/java/co/edu/poli/ces3/pitagorasapi/Servlet/ServletCursos.java
+++ src/main/java/co/edu/poli/ces3/pitagorasapi/Servlet/ServletCursos.java
@@ -1,14 +1,21 @@
 package co.edu.poli.ces3.pitagorasapi.Servlet;
 
 import co.edu.poli.ces3.pitagorasapi.DAO.Cursos;
+import com.google.gson.Gson;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.rmi.ServerException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 
-@WebServlet(name = "servletcurso", value = {"/cursos" , "/cursos/facultad"})
+@WebServlet(name = "servletcurso", value = {"/cursos" , "/cursos/facultad" , "/cursos/ruta"})
 public class ServletCursos extends HttpServlet {
 
     private ArrayList<Cursos> listaCursos;
@@ -69,5 +76,68 @@ public class ServletCursos extends HttpServlet {
         ));
 
     }
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        Gson gson = new Gson();
+
+        String path = req.getServletPath();
+
+        if (path.equals("/cursos")) {
+            if (req.getParameter("id") == null) {
+                out.print(gson.toJson(listaCursos));
+            }else{
+                int id = Integer.parseInt(req.getParameter("id"));
+                Cursos BuscarCurso = null;
+                for (Cursos curso : listaCursos) {
+                    if (curso.getId() == id) {
+                        BuscarCurso = curso;
+                        break;
+                    }
+                }
+                if (BuscarCurso != null) {
+                    out.print(gson.toJson(BuscarCurso));
+                } else {
+                    out.print("{error\":\"No se encontro el curso\"}");
+                }
+            }
+
+
+
+        } else if (path.equals("/cursos/facultad")) {
+            if (req.getParameter("nombre") == null) {
+                out.print(gson.toJson(listaCursos));
+            } else {
+                String nombre = req.getParameter("nombre");
+                List<Cursos> CursosEnFacultad = BuscarCursosporFacultad(nombre);
+                for (Cursos curso : CursosEnFacultad) {
+                    out.print(gson.toJson(curso));
+                }
+            }
+
+        }
+
+    }
+
+
+
+    private List<Cursos> BuscarCursosporFacultad(String facultad) {
+        List<Cursos> listaCursosenFacultades = new ArrayList<>();
+        for (Cursos curso : listaCursos) {
+            if (curso.getFacultad().equalsIgnoreCase(facultad)) {
+                listaCursosenFacultades.add(curso);
+            }
+        }
+        return listaCursosenFacultades;
+    }
+
+
+
 
 }
